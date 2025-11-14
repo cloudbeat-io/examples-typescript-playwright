2776af2d182636698e5823cc25122e86ba7b9a08
romovs <romovs@gmail.com>
2025-11-14 13:37:34 +0200

    Remove firefox

--- playwright.config.ts
+++ playwright.config.ts
@@ -18,10 +18,7 @@ export default defineConfig({
       use: { ...devices['Desktop Chrome'] },
     },
 
-    {
-      name: 'firefox',
-      use: { ...devices['Desktop Firefox'] },
-    },
+
 
     {
       name: 'webkit',
